8ce9dc1b9bf5406c934b3ce78ba430b7b514a205
marlychietton <charly.mietton@gmail.com>
2020-04-12 15:09:22 +0200

    pep-8, using classes attributes...

--- Vistalli_Sally_Python.py
+++ Vistalli_Sally_Python.py
@@ -1,21 +1,27 @@
-#VISTALLI SALLY 
-#11612640
+# VISTALLI SALLY
+# 11612640
 
 import random
+from copy import deepcopy
+
 import matplotlib.pyplot as plt
 
 #######CITOYEN
 
+
 class Citoyen:
     def __init__(self):
-        self.ncontact = random.normalvariate(7,4)
+        self.ncontact = random.normalvariate(7, 4)
         self.sante = "sain"
         self.joursMalade = 0
 
     def malade(self):
+        """
+        Rend le citoyen malade.
+        """
         self.sante = "malade"
         self.joursMalade = 1
-        
+
     def critique(self):
         self.sante = "critique"
         self.ncontact = 0
@@ -27,141 +33,147 @@ class Citoyen:
         return str(self.sante) + str(self.ncontact)
 
     def copy(self):
-        c = Citoyen()
-        c.sante = self.sante
-        c.ncontact = self.ncontact
-        c.joursMalade = self.joursMalade
-        return c
+        return deepcopy(self)
 
 
 ############POPULATION
 
+
 class Population:
-    def __init__(self,n=1000):
+    def __init__(self, n=1000):
         self.jour = 0
-        self.D = 0
+        self.D = 0  # probabilité qu'un malade soit
         self.Pi = 0
         self.Pc = 0
         self.nContactInf = 0
         self.n = n
         self.lPop = [Citoyen() for i in range(n)]
-        self.store = {
+        self.updateStatSante()
+        self.progression = []  # chaque élement du tableau correspond à un jour simulé
+
+    def updateStatSante(self):
+        self.statSante = {
             "sain": 0,
             "malade": 0,
             "critique": 0,
             "immunise": 0,
         }
-
-    def statSante(self):
-        li = [0]*4
-        for Citoyen in self.lPop:
-            if Citoyen.sante== "sain":
-                li[0]+=1
-            elif Citoyen.sante== "malade":
-                li[1]+=1
-            elif Citoyen.sante== "critique":
-                li[2]+=1
-            else:
-                li[3]+=1
-        self.store["sain"] = li[0]
-        self.store["malade"] = li[1]
-        self.store["critique"] = li[2]
-        self.store["immunise"] = li[3]
-        return li
+        for citoyen in self.lPop:
+            self.statSante[citoyen.sante] += 1
 
     def __str__(self):
-        return ("Jours:" + str(self.jour)+"\n Population de "+str(self.n)+" citoyens"+"\n Sains:"+str(self.store["sain"])+"\n Malades:"+str(self.store["malade"])+"\n Critiques:"+str(self.store["critique"])+"\n Immunises:"+str(self.store["immunise"]))
+        return (
+            "Jours:"
+            + str(self.jour)
+            + "\n\tPopulation de "
+            + str(self.n)
+            + " citoyens"
+            + "\n\tSains:"
+            + str(self.statSante["sain"])
+            + "\n\tMalades:"
+            + str(self.statSante["malade"])
+            + "\n\tCritiques:"
+            + str(self.statSante["critique"])
+            + "\n\tImmunises:"
+            + str(self.statSante["immunise"])
+        )
 
-    def infection(self,D,Pi,Pc):
+    def infection(self, D, Pi, Pc):
         self.D = D
         self.Pi = Pi
         self.Pc = Pc
-        self.lPop[0].malade()
-        self.nContactInf = self.lPop[0].ncontact 
+        first_citizen = self.lPop[0]
+        # make him sick
+        first_citizen.malade()
+        # at start of infection set number of infected people
+        self.nContactInf = first_citizen.ncontact
 
     def updatePop(self):
         self.jour += 1
-        for Citoyen in self.lPop:
+        for citoyen in self.lPop:
             r = random.random()
-            if Citoyen.sante == "sain" and r < self.Pi*(self.nContactInf/self.n):
-                Citoyen.malade()
-                self.nContactInf += Citoyen.ncontact
-            elif Citoyen.sante == "malade":
-                if Citoyen.joursMalade == self.D:
-                    Citoyen.immunise()
-                    self.nContactInf -= Citoyen.ncontact
+            # probility that the citizen is infected
+            is_infected = r < self.Pi * (self.nContactInf / self.n)
+            # update citizen if citizen has been infected
+            if citoyen.sante == "sain" and is_infected:
+                citoyen.malade()
+                self.nContactInf += citoyen.ncontact
+            elif citoyen.sante == "malade":
+                if citoyen.joursMalade == self.D:
+                    citoyen.immunise()
+                    self.nContactInf -= citoyen.ncontact
                 else:
-                    Citoyen.joursMalade += 1
+                    citoyen.joursMalade += 1
                     if r < self.Pc:
-                        self.nContactInf -= Citoyen.ncontact
-                        Citoyen.critique()
+                        self.nContactInf -= citoyen.ncontact
+                        citoyen.critique()
 
-    def passeJours(self,jours):
+    def passeJours(self, jours):
         for i in range(jours):
             self.updatePop()
 
-    def confinementEtHygiene(self,d=2):
-        self.Pi = self.Pi/2
+    def confinementEtHygiene(self, d=2):
+        self.Pi = self.Pi / 2
         self.nContactInf = 0
-        for Citoyen in self.lPop:
-            Citoyen.ncontact=Citoyen.ncontact/d
-            if Citoyen.sante == "malade":
-                self.nContactInf += Citoyen.ncontact
+        for citoyen in self.lPop:
+            citoyen.ncontact = citoyen.ncontact / d
+            if citoyen.sante == "malade":
+                self.nContactInf += citoyen.ncontact
 
     def copy(self):
-        p = Population()
-        p.jour = self.jour
-        p.D = self.D
-        p.Pi = self.Pi
-        p.Pc = self.Pc
-        p.nContactInf = self.nContactInf
-        p.n = self.n
-        for i in range(self.n):
-            p.lPop[i]=self.lPop[i].copy()
-        return p
+        return deepcopy(self)
 
-    def simulate(self,jours,jour_confinement_start=None,valeur_confinement=None):
-        li = []
-        if jour_confinement_start==None:
-            for i in range(jours+1):
+    def simulate(self, jours, jour_confinement_start=None, valeur_confinement=None):
+        if jour_confinement_start == None:
+            for i in range(jours + 1):
                 self.passeJours(1)
-                li.append(self.statSante())
+                self.updateStatSante()
+                self.progression.append(self.statSante)
         else:
-            for i in range(jours+1):
+            for i in range(jours + 1):
                 self.passeJours(1)
-                if jour_confinement_start==i:
+                if jour_confinement_start == i:
                     self.confinementEtHygiene(valeur_confinement)
-                li.append(self.statSante())
-        return li
+                self.updateStatSante()
+                self.progression.append(self.statSante)
 
-    def plot(self,jours,jour_confinement_start=None,valeur_confinement=None):
-        L=self.simulate(jours,jour_confinement_start,valeur_confinement)
-        N=self.n
-        S=[i[0]/N for i in L]
-        M=[i[1]/N for i in L]
-        C=[i[2]/N for i in L]
-        I=[i[3]/N for i in L]
-        plt.plot(range(jours+1),S,color='black',label='Sain')
-        plt.plot(range(jours+1),I,color='green',label='Immunise')
-        plt.plot(range(jours+1),M,color='orange',label='Malade')
-        plt.plot(range(jours+1),C,color='red',label='Critique')
+    def plot(self, jours, jour_confinement_start=None, valeur_confinement=None):
+        self.simulate(jours, jour_confinement_start, valeur_confinement)
+        N = self.n  # pas utile en soi
+        # y'aurait une petite opti à faire ici (on itère 4 fois), mais pas sur que ce soit utile
+        # le nombre de jour n'est jamais vraiment très grand
+        S = [i["sain"] / N for i in self.progression]
+        M = [i["malade"] / N for i in self.progression]
+        C = [i["critique"] / N for i in self.progression]
+        I = [i["immunise"] / N for i in self.progression]
+        c = (
+            ["blue"] * (jours + 1)
+            + ["grey"] * (jours + 1)
+            + ["orange"] * (jours + 1)
+            + ["red"] * (jours + 1)
+        )
+        fig, ax = plt.subplots()
+        scatter = ax.scatter([range(jours + 1)] * 4, [S, M, C, I], c=c)
+        print(scatter.legend_elements())
+        #        legend1 = ax.legend([',
+        #                    loc="upper right", title="Classes")
+        #        ax.add_artist(legend1)
         plt.legend()
         plt.show()
 
 
-
 #######TEST CODES 1
-p=Population()
-p.infection(10, 0.05, 0.02) # L'infection dure 10 jours,
+p = Population()
+p.infection(10, 0.05, 0.02)  # L'infection dure 10 jours,
 # la probabilite de contaminer est de 5%
 # la probabilite de passer dans un etat critique est de 2%
 # (proche du coronavirus)
 # Avec 7 contacts en moyenne, R0 = 7 * 0.05 * 10 = 3.5
 p.passeJours(2)
 print(p)
-p1=p.copy() # Pas de confinement au jour 2, confinement leger au jour 4
-p2=p.copy() # confinement leger au jour 2
-p3=p.copy() # confinement lourd au jour 2
+p1 = p.copy()  # Pas de confinement au jour 2, confinement leger au jour 4
+p2 = p.copy()  # confinement leger au jour 2
+p3 = p.copy()  # confinement lourd au jour 2
 # L'intensite du confinement peut ^etre determine avec le parametre d de la methode
 # confinementEtHygiene :
 # 2 reduit de moitie les interaction (mesures legeres)
@@ -189,14 +201,13 @@ print("\np3 :")
 print(p3)
 
 #######TEST CODE 2
-p=Population()
+p = Population()
 p.infection(10, 0.05, 0.02)
-print(p.simulate(30,10,3)) # simuler l'evolution pendant 80 jours avec un
-#confinement de valeur 3 applique au jour 10
+print(p.simulate(30, 10, 3))  # simuler l'evolution pendant 80 jours avec un
+# confinement de valeur 3 applique au jour 10
 
 ###PLOT####
-p=Population()
+p = Population()
 p.infection(10, 0.05, 0.02)
-p.plot(80,20,2) # simule et trace l'evolution pendant 80 jours avec un
-#confinement de valeur 2 applique au jour 20
-
+p.plot(80, 20, 2)  # simule et trace l'evolution pendant 80 jours avec un
+# confinement de valeur 2 applique au jour 20
